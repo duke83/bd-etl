e9ec72717945cdc8e0813840625f90435beadebd
Brant Merrell <brant.merrell@gmail.com>
2016-01-15 10:55:05 -0500

    function nrow

--- util.ts
+++ util.ts
@@ -8,25 +8,28 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3({s3ForcePathStyle: true});
 var parse = require('csv-parse');
 
-var s3FilenameParams = {
-    Bucket: 'fdic_stage_3',
-    Key: filename
-};
-var startTime=Date.now();
-var parser = parse({delimiter: ','});
-parser.on('readable', function () {
-    while (parser.read()) {}
-});
-parser.on('error', function (err) {
-    console.log(err.message);
-});
-var readstream = s3.getObject(s3FilenameParams).createReadStream();
-readstream.on('readable', function () {
-    parser.write(readstream.read());
-});
-readstream.on('end', function () {
-    var endTime=Date.now();
-    console.log('total',(endTime-startTime)/1000);
-    console.log('parser.count',parser.count);
-    context.succeed(parser.count);
-});
+function nrow(filename){
+    var s3FilenameParams = {
+        Bucket: 'fdic_stage_3',
+        Key: filename
+    };
+    var startTime=Date.now();
+    var parser = parse({delimiter: ','});
+    parser.on('readable', function () {
+        while (parser.read()) {}
+    });
+    parser.on('error', function (err) {
+        console.log(err.message);
+    });
+    var readstream = s3.getObject(s3FilenameParams).createReadStream();
+    readstream.on('readable', function () {
+        parser.write(readstream.read());
+    });
+    readstream.on('end', function () {
+        var endTime=Date.now();
+        console.log('total',(endTime-startTime)/1000);
+        console.log('parser.count',parser.count);
+        context.succeed(parser.count);
+    });
+}
+
